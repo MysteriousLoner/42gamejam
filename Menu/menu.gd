4b771529d776956220179c73e20d1f7a30ef8d9d
SummerNatsuha (Ethan) <148058160+natsuha-etoile@users.noreply.github.com>
2023-11-24 16:43:55 +0800

    menu3

---
+++ Menu/menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_StartButton_pressed():
+	get_tree().change_scene("")
+	
+func _on_OptionsButton_pressed():
+	var options = load("").instance()
+	get_tree().current_scene.add_child(options)
+	
+func _on_QuitButton_pressed():
+	get_tree().quit()
